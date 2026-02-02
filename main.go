0b2541fd70c92b4be0b92a909629e46372d9e519
Peuraud-Pierre <peuraudpierre9@gmail.com>
2026-02-02 17:07:06 +0100

    r

--- main.go
+++ main.go
@@ -38,10 +38,7 @@ type HealthResponse struct {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		allowedOrigins := os.Getenv("ALLOWED_ORIGINS")
-		if allowedOrigins == "" {
-			allowedOrigins = "*"
-		}
+		allowedOrigins := "http://localhost:8082/, https://vintagestandards.fr, https://dev.vintagestandards.fr"
 		w.Header().Set("Access-Control-Allow-Origin", allowedOrigins)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
