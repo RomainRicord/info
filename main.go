d65b394e0bacc797b81e2f0ef95174e1b5b9320f
Peuraud-Pierre <peuraudpierre9@gmail.com>
2026-02-02 17:00:15 +0100

    r

--- main.go
+++ main.go
@@ -45,7 +45,6 @@ func corsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", allowedOrigins)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Access-Control-Max-Age", "3600")
 
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
